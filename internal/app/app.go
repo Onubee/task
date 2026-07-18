9e9532df43a10314fbe83b71dc743b08a9623d9a
onube <onubee@yandex.ru>
2026-07-18 18:29:13 +0300

    connecting logger

--- internal/app/app.go
+++ internal/app/app.go
@@ -1,29 +1,35 @@
 package app
 
 import (
-	"fmt"
-
 	"github.com/Onubee/task/internal/config"
+	"github.com/Onubee/task/internal/logger"
 )
 
 type App struct {
 	config *config.Config
+	logger *logger.Logger // ← добавили логгер
 }
 
 // NewApp — создает новое приложение
 func NewApp(cfg *config.Config) *App {
+	// Создаем логгер с уровнем из конфига
+	log := logger.NewLogger(cfg.LogLevel)
+
 	return &App{
 		config: cfg,
+		logger: log,
 	}
 }
 
 // Run — запускает приложение
 func (a *App) Run() error {
-	fmt.Println("🚀 Application started!")
-	fmt.Printf("📦 Config: DB=%s:%d, Server=:%d\n",
+	a.logger.Info("🚀 Application started!")
+	a.logger.Info("📦 Config: DB=%s:%d, Server=:%d",
 		a.config.DBHost,
 		a.config.DBPort,
 		a.config.ServerPort,
 	)
+	a.logger.Debug("🔍 Product sources: %v", a.config.GetProductURLs())
+
 	return nil
 }
